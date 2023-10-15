9fe1a498321b9f084a6dac1c0721f1fe1c72face
Jie-Green <ryokujie48@gmail.com>
2023-10-15 11:50:31 +0900

    fainal commit

--- RenTumBrella.py
+++ RenTumBrella.py
@@ -3,8 +3,11 @@ from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import LoginManager, login_user, logout_user, login_required, UserMixin
 
+# Initialization
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///rentumbrella.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.secret_key = "your_unique_secret_key_here"
 
 db = SQLAlchemy(app)
 
@@ -13,11 +16,7 @@ login_manager.init_app(app)
 login_manager.login_view = "login"
 
 
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
-
-
+# Models
 class Umbrella(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     design = db.Column(db.String(80), unique=True, nullable=False)
@@ -30,6 +29,12 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(120), nullable=False)
 
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+# Routes
 @app.route("/")
 def index():
     umbrellas = Umbrella.query.all()
@@ -42,6 +47,7 @@ def register():
         username = request.form.get("username")
         password = request.form.get("password")
         existing_user = User.query.filter_by(username=username).first()
+
         if existing_user:
             flash("Username already exists. Choose a different one.")
             return redirect(url_for("register"))
@@ -61,11 +67,14 @@ def register():
 def login():
     if request.method == "POST":
         user = User.query.filter_by(username=request.form["username"]).first()
+
         if user and check_password_hash(user.password, request.form["password"]):
             login_user(user, remember=True)
             return redirect(url_for("index"))
         else:
-            return "Incorrect username or password"
+            flash("Incorrect username or password")
+            return redirect(url_for("login"))
+
     return render_template("login.html")
 
 
@@ -81,34 +90,65 @@ def catalog():
     umbrellas = Umbrella.query.all()
     return render_template("catalog.html", umbrellas=umbrellas)
 
+
 @app.route("/rent_umbrella/<int:umbrella_id>")
 @login_required
 def rent_umbrella(umbrella_id):
     umbrella = Umbrella.query.get_or_404(umbrella_id)
-    
+
     if umbrella.stock <= 0:
         flash("This umbrella is not available right now.")
         return redirect(url_for("catalog"))
 
-    # Stock 減少
     umbrella.stock -= 1
     db.session.commit()
 
     flash(f"You have rented the {umbrella.design} umbrella.")
     return redirect(url_for("index"))
 
+
 @app.route("/return_umbrella/<int:umbrella_id>")
 @login_required
 def return_umbrella(umbrella_id):
     umbrella = Umbrella.query.get_or_404(umbrella_id)
 
-    # Stock 増加
     umbrella.stock += 1
     db.session.commit()
 
     flash(f"You have returned the {umbrella.design} umbrella.")
     return redirect(url_for("index"))
 
+
+@app.route("/add_umbrella", methods=["GET", "POST"])
+@login_required  # このアクションを実行するにはログインが必要です。
+def add_umbrella():
+    if request.method == "POST":
+        design = request.form.get("design")
+        stock = request.form.get("stock")
+        new_umbrella = Umbrella(design=design, stock=stock)
+        db.session.add(new_umbrella)
+        db.session.commit()
+        flash("新しい傘が追加されました。")
+        return redirect(url_for("index"))
+
+    return render_template("add_umbrella.html")
+
+
+@app.route("/delete_umbrella/<int:umbrella_id>", methods=["GET", "POST"])
+@login_required
+def delete_umbrella(umbrella_id):
+    umbrella = Umbrella.query.get_or_404(umbrella_id)
+
+    if request.method == "POST":
+        db.session.delete(umbrella)
+        db.session.commit()
+        flash(f"{umbrella.design}傘が削除されました。")
+        return redirect(url_for("catalog"))
+
+    return render_template("delete_umbrella.html", umbrella=umbrella)
+
+
+# Main execution
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
---
+++ static/script.js
@@ -0,0 +1,5 @@
+// static/script.js
+
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("ページが正しく読み込まれました！");
+});
